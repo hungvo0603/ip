70c4e1f38ec909d7ecc29add4348edcb889bf8f0
hungvo0603 <hungvo1011@gmail.com>
2020-08-25 20:26:15 +0800

    Update Duke.java to Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,6 +4,8 @@ public class Duke {
 
     public static void main(String[] args) {
         String line;
+        String[] tasks = new String[100];
+        int taskCount = 0;
         Scanner in = new Scanner(System.in);
 
         System.out.println("The You-know-who:");
@@ -12,9 +14,20 @@ public class Duke {
         System.out.print("Hung:\n\t");
         line = in.nextLine();
         while (!line.equalsIgnoreCase("bye")){
-            System.out.println("The You-know-who:\n\t" + line);
-            System.out.print("Hung:\n\t");
-            line = in.nextLine();
+            if (line.equalsIgnoreCase("list")) {
+                System.out.println("The You-know-who:");
+                for(int i = 1; i < taskCount + 1; i = i + 1){
+                    System.out.println("\t" + i + ". " + tasks[i - 1]);
+                }
+                System.out.print("Hung:\n\t");
+                line = in.nextLine();
+            } else {
+                tasks[taskCount] = line;
+                taskCount++;
+                System.out.println("The You-know-who:\n\tadded: " + line);
+                System.out.print("Hung:\n\t");
+                line = in.nextLine();
+            }
         }
 
         System.out.println("The You-know-who:");
