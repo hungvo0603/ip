8785a8ec26d0d0b43176ddb60bf87294531d531e
hungvo0603 <hungvo1011@gmail.com>
2020-09-15 20:57:33 +0800

    Add "delete" task function

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -8,50 +8,47 @@ import duke.task.Event;
 import duke.task.Task;
 import duke.task.Todo;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
 
     private static Command commandType;
-    private static final int MAX_TASKS = 100;
+
+    private static final ArrayList<Task> tasks = new ArrayList<>();
+
     private static final Scanner SCANNER = new Scanner(System.in);
 
-    public static void addDeadlineTask (Task[] tasks, String command, String delimiter) {
+    public static void addDeadlineTask (String command, String delimiter) {
         try {
             String description = getDeadlineAndEventDescription(command, delimiter);
             String time = getDeadlineAndEventTime(command, delimiter);
-            int taskCount = Task.getNumberOfTask();
             Task t = new Deadline(description, time);
-            tasks[taskCount] = t;
-            printAddMessage(tasks[taskCount]);
-            Task.incrementNumberOfTask();
+            tasks.add(t);
+            printAddMessage(t);
         } catch (DukeException e) {
             ErrorMessage.printDeadlineSyntaxCommandMessage(command);
         }
     }
 
-    public static void addEventTask (Task[] tasks, String command, String delimiter) {
+    public static void addEventTask (String command, String delimiter) {
         try {
             String description = getDeadlineAndEventDescription(command, delimiter);
             String time = getDeadlineAndEventTime(command, delimiter);
-            int taskCount = Task.getNumberOfTask();
             Task t = new Event(description, time);
-            tasks[taskCount] = t;
-            printAddMessage(tasks[taskCount]);
-            Task.incrementNumberOfTask();
+            tasks.add(t);
+            printAddMessage(t);
         } catch (DukeException e) {
             ErrorMessage.printEventSyntaxCommandMessage(command);
         }
     }
 
-    public static void addTodoTask (String command, Task[] tasks) {
+    public static void addTodoTask (String command) {
         try {
             String description = getTodoDescription(command);
             Task t = new Todo(description);
-            int taskCount = Task.getNumberOfTask();
-            tasks[taskCount] = t;
-            printAddMessage(tasks[taskCount]);
-            Task.incrementNumberOfTask();
+            tasks.add(t);
+            printAddMessage(t);
         } catch (DukeException e) {
             ErrorMessage.printTodoSyntaxCommandMessage(command);
         }
@@ -138,6 +135,9 @@ public class Duke {
         case "done":
             commandType = Command.DONE;
             break;
+        case "delete":
+            commandType = Command.DELETE;
+            break;
         default:
             commandType = Command.ERROR;
             throw new DukeException();
@@ -159,11 +159,11 @@ public class Duke {
     }
 
     public static void printAddMessage (Task task) {
-        int taskCount = Task.getNumberOfTask() + 1;
+        int taskCount = tasks.size();
         System.out.println("-----------------------------------------");
         System.out.println("!bot:\nAdded: " + task.toString());
         if (taskCount <= 1) {
-            System.out.println("You now have " + taskCount + " duke.task in the list.");
+            System.out.println("You now have " + taskCount + " task in the list.");
         } else {
             System.out.println("You now have " + taskCount + " tasks in the list.");
         }
@@ -173,41 +173,53 @@ public class Duke {
     public static void printSetTaskDoneMessage (Task task) {
         System.out.println("-----------------------------------------");
         System.out.println("!bot:");
-        System.out.println("Nice! I've mark this duke.task as done:");
+        System.out.println("Nice! I've mark this task as done:");
         System.out.printf(" %s\n", task.toString());
         System.out.println("-----------------------------------------");
     }
 
-    public static void printTaskList (Task[] tasks) {
-        int taskCount = Task.getNumberOfTask();
+    public static void printTaskList (ArrayList<Task> tasks) {
+        int taskCount = tasks.size();
         System.out.println("-----------------------------------------");
         System.out.println("!bot:");
 
         if (taskCount == 0) {
-            System.out.println("There is no duke.task in the list for now.");
+            System.out.println("There is no task in the list for now.");
             System.out.println("-----------------------------------------");
             return;
         }
 
         for (int i = 1; i < taskCount + 1; i = i + 1) {
-            System.out.printf("%d. %s\n", i, tasks[i - 1].toString());
+            System.out.printf("%d. %s\n", i, tasks.get(i-1).toString());
         }
+
         System.out.println("-----------------------------------------");
     }
 
-    public static void setTaskAsDone (String command, Task[] tasks) {
+    public static void deleteTask (String command) {
         try {
-            int taskNumber = Integer.parseInt(command.substring(5)) - 1;
-            tasks[taskNumber].setDone();
-            printSetTaskDoneMessage(tasks[taskNumber]);
+            int taskNumber = Integer.parseInt(command.substring(7)) - 1;
+            tasks.remove(taskNumber);
         } catch (NumberFormatException e) {
             ErrorMessage.printNumberFormatErrorMessage();
-        } catch (NullPointerException | ArrayIndexOutOfBoundsException e) {
+        } catch (NullPointerException | IndexOutOfBoundsException e) {
             ErrorMessage.printOutOfBoundsErrorMessage();
         }
     }
 
-    public static void executeCommands (Task[] tasks) {
+    public static void setTaskAsDone (String command) {
+        try {
+            int taskNumber = Integer.parseInt(command.substring(5)) - 1;
+            tasks.get(taskNumber).setDone();
+            printSetTaskDoneMessage(tasks.get(taskNumber));
+        } catch (NumberFormatException e) {
+            ErrorMessage.printNumberFormatErrorMessage();
+        } catch (NullPointerException | IndexOutOfBoundsException e) {
+            ErrorMessage.printOutOfBoundsErrorMessage();
+        }
+    }
+
+    public static void executeCommands () {
         String command;
         command = getInput();
 
@@ -217,16 +229,19 @@ public class Duke {
                 printTaskList(tasks);
                 break;
             case DONE:
-                setTaskAsDone(command, tasks);
+                setTaskAsDone(command);
                 break;
             case TODO:
-                addTodoTask(command, tasks);
+                addTodoTask(command);
                 break;
             case DEADLINE:
-                addDeadlineTask(tasks, command, "/by");
+                addDeadlineTask(command, "/by");
                 break;
             case EVENT:
-                addEventTask(tasks, command, "/at");
+                addEventTask(command, "/at");
+                break;
+            case DELETE:
+                deleteTask(command);
                 break;
             default:
                 break;
@@ -236,9 +251,8 @@ public class Duke {
     }
 
     public static void main (String[] args) {
-        Task[] tasks = new Task[MAX_TASKS];
         printWelcomeMessage();
-        executeCommands(tasks);
+        executeCommands();
         printGoodbyeMessage();
     }
 
